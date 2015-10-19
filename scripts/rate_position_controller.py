851a5c0e41ee87e3c8d6db6829fc3df5502e5714
lrubior88 <l.rubiorod@gmail.com>
2015-10-19 12:01:59 +0200

    Fixed orientation problems in ratae_position_controller

--- scripts/rate_position_controller.py
+++ scripts/rate_position_controller.py
@@ -16,7 +16,7 @@ from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 from baxter_core_msgs.msg import EndpointState
 from omni_msgs.msg import OmniState, OmniFeedback, OmniButtonEvent
-from geometry_msgs.msg import Vector3, Quaternion, Transform, PoseStamped
+from geometry_msgs.msg import Vector3, Point, PoseStamped, Quaternion, Wrench, Transform, PoseStamped
 from visualization_msgs.msg import Marker
 # State Machine
 import smach
@@ -26,6 +26,12 @@ from smach import CBState
 from math import pi, exp, sin, sqrt
 import numpy as np
 import tf.transformations as tr
+# Quaternions tools
+import PyKDL
+
+GREY_BUTTON = 0
+WHITE_BUTTON = 1
+GRIPPER_RATIO = 10
 
 class TextColors:
   HEADER = '\033[95m'
@@ -70,6 +76,8 @@ class RatePositionController:
     self.slave_state_topic = '/%s/state' % slave_name
     self.ik_mc_topic = '/%s/ik_command' % slave_name
     self.gripper_topic = '/%s/GRIP/command' % slave_name
+    self.button_topic = '/%s/button' % master_name
+        
     # Workspace definition
     self.units = self.read_parameter('~units', 'mm')
     width = self.read_parameter('~workspace/width', 140.0)
@@ -79,27 +87,52 @@ class RatePositionController:
     self.workspace = np.array([width, depth, height])
     self.hysteresis = self.read_parameter('~hysteresis', 3.0)
     self.pivot_dist = self.read_parameter('~pivot_dist', 5.0)
+    
     # Force feedback parameters
     self.k_center = self.read_parameter('~k_center', 0.1)
     self.b_center = self.read_parameter('~b_center', 0.003)
     self.k_rate = self.read_parameter('~k_rate', 0.05)
     self.b_rate = self.read_parameter('~b_rate', 0.003)
+    
     # Position parameters
-    self.rpy_offset = self.read_parameter('~rpy_offset', [0,0,0])
-    self.q_offset = tr.quaternion_from_euler(*self.rpy_offset)
+    self.hysteresis = self.read_parameter('~hysteresis', 3.0)
+    self.pivot_dist = self.read_parameter('~pivot_dist', 5.0)
     self.publish_frequency = self.read_parameter('~publish_rate', 1000.0)
     self.position_ratio = self.read_parameter('~position_ratio', 250)
+    self.axes_rotation = self.read_parameter('~axes_rotation', [0, 0, 0])
+    self.angle_rotation = self.read_parameter('~angle_rotation', 1.570796)
+    self.position_axes = [0, 1, 2]
+    self.position_sign = np.array([1.0, 1.0, 1.0])
+    self.axes_mapping = self.read_parameter('~axes_mapping', ['x', 'y' ,'z'])
+    
+    rospy.logwarn('axes_mapping[0] -> %s' % self.axes_mapping[0])
+    rospy.logwarn('axes_mapping[1] -> %s' % self.axes_mapping[1])
+    rospy.logwarn('axes_mapping[2] -> %s' % self.axes_mapping[2])
+    
+    if len(self.axes_mapping) != 3:
+      rospy.logwarn('The invalid number of values in [axes_mapping]. Received 3, expected %d' % len(self.axes_mapping))
+    for i, axis in enumerate(self.axes_mapping):
+      axis = axis.lower()
+      if '-' == axis[0]:
+        axis = axis[1:]
+        self.position_sign[i] = -1.0
+      if axis not in ('x','y','z'):
+        rospy.logwarn('Invalid axis %s given in [axes_mapping]' % axis)
+      self.position_axes[i] = ['x','y','z'].index(axis)
+    
     # Vibration parameters
     self.vib_a = self.read_parameter('~vibration/a', 2.0)             # Amplitude (mm)
     self.vib_c = self.read_parameter('~vibration/c', 5.0)             # Damping
     self.vib_freq = self.read_parameter('~vibration/frequency', 30.0) # Frequency (Hz)
     self.vib_time = self.read_parameter('~vibration/duration', 0.3)   # Duration (s)
     self.vib_start_time = 0.0
+    
     # Rate parameters
     self.rate_pivot = np.zeros(3)
     self.rate_gain = self.read_parameter('~rate_gain', 1.0) 
     
     # Initial values
+    self.center_pos = np.array([0, 0, 0])
     self.frame_id = self.read_parameter('~reference_frame', 'world')
     self.colors = TextColors()
     self.gripper_cmd = 0.0
@@ -111,10 +144,16 @@ class RatePositionController:
     self.slave_rot = np.array([0, 0, 0, 1])
     self.timer = None
     self.force_feedback = np.zeros(3)
+    
     # Synch
     self.slave_synch_pos = np.zeros(3)
     self.slave_synch_rot = np.array([0, 0, 0, 1])
     self.master_synch_rot = np.array([0, 0, 0, 1])
+    
+    # Button
+    self.prev_buttons = [0] * 2
+    self.buttons = [False] * 2
+    self.buttons[WHITE_BUTTON] = True
     
     # Setup Subscribers/Publishers
     self.feedback_pub = rospy.Publisher(self.feedback_topic, OmniFeedback)
@@ -123,6 +162,7 @@ class RatePositionController:
     self.vis_pub = rospy.Publisher('visualization_marker', Marker)
     rospy.Subscriber(self.master_state_topic, OmniState, self.cb_master_state)
     rospy.Subscriber(self.slave_state_topic, EndpointState, self.cb_slave_state)
+    rospy.Subscriber(self.button_topic, OmniButtonEvent, self.buttons_cb)
     
     self.loginfo('Waiting for [%s] and [%s] topics' % (self.master_state_topic, self.slave_state_topic))
     while not rospy.is_shutdown():
@@ -163,12 +203,10 @@ class RatePositionController:
   def position_control(user_data, self):
     if self.inside_workspace(self.master_pos):
       self.command_pos = self.slave_synch_pos + self.master_pos / self.position_ratio
-      # TODO: Rotations are driving me crazy! 
-      relative_rot = tr.quaternion_multiply(self.master_synch_rot, tr.quaternion_inverse(self.master_rot))
-      self.command_rot = tr.quaternion_multiply(self.slave_synch_rot, relative_rot)
-      #~ self.command_rot = np.array(self.master_rot)
+      self.command_rot = np.array(self.master_rot)
       return 'stay'
     else:
+      self.force_feedback = np.zeros(3)
       self.command_pos = np.array(self.slave_pos)
       self.command_rot = np.array(self.slave_rot)
       self.vib_start_time = rospy.get_time()
@@ -198,8 +236,7 @@ class RatePositionController:
       # Send the rate command to the slave
       distance = sqrt(np.sum((self.master_pos - self.rate_pivot) ** 2)) / self.position_ratio
       self.command_pos += (self.rate_gain * distance * self.normalize_vector(self.master_pos)) / self.position_ratio
-      relative_rot = tr.quaternion_multiply(self.master_synch_rot, tr.quaternion_inverse(self.master_rot))
-      self.command_rot = tr.quaternion_multiply(self.slave_synch_rot, relative_rot)
+      self.command_rot = np.array(self.master_rot)
       return 'stay'
     else:
       self.command_pos = np.array(self.slave_pos)
@@ -235,53 +272,77 @@ class RatePositionController:
     
   def normalize_vector(self, v):
     result = np.array(v)
-    result /= np.sqrt(np.sum((result ** 2)))
+    norm = np.sqrt(np.sum((result ** 2)))
+    if norm:
+      result /= norm
+    return result
+    
+  def change_axes(self, array, index=None, sign=None):
+    if index == None:
+      index = self.position_axes
+    if sign == None:
+      sign = self.position_sign
+    result = np.zeros(len(array))
+    for i, idx in enumerate(index):
+      result[i] = array[idx] * sign[idx]
+    return result
+    
+  def change_force_axes(self, array, index=None, sign=None):
+    if index == None:
+      index = self.position_axes
+    if sign == None:
+      sign = self.position_sign
+    result = np.zeros(len(array))
+    for i, idx in enumerate(index):
+      result[i] = array[idx] * sign[i] #~ ??
     return result
   
   def send_feedback(self):
     feedback_msg = OmniFeedback()
-    feedback_msg.force.x = self.force_feedback[0]
-    feedback_msg.force.y = self.force_feedback[1]
-    feedback_msg.force.z = self.force_feedback[2]
-    feedback_msg.position.x = self.center_pos[0]
-    feedback_msg.position.y = self.center_pos[1]
-    feedback_msg.position.z = self.center_pos[2]
-    self.feedback_pub.publish(feedback_msg)
+    force = self.change_force_axes(self.force_feedback)
+    pos = self.change_axes(self.center_pos)
+    feedback_msg.force = Vector3(*force)
+    feedback_msg.position = Vector3(*pos)
+    self.feedback_pub.publish(feedback_msg) 
   
   # DO NOT print to the console within this function
   def cb_master_state(self, msg):    
-    self.master_pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]) - self.center_pos
-    self.master_rot = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
-    self.master_rot = tr.quaternion_multiply(self.q_offset, self.master_rot)
-    self.master_vel = np.array([msg.velocity.x, msg.velocity.y, msg.velocity.z])
+    self.master_real_pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
+    pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]) - self.center_pos
+    vel = np.array([msg.velocity.x, msg.velocity.y, msg.velocity.z])
+    self.master_pos = self.change_axes(pos)
+    self.master_vel = self.change_axes(vel)
+    
+    # Rotate tu use the same axes orientation between master and slave
+    real_rot = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+    q = tr.quaternion_about_axis(self.angle_rotation, self.axes_rotation)
+    self.master_rot = tr.quaternion_multiply(real_rot, q)
     self.master_dir = self.normalize_vector(self.master_vel)
-    # Control the gripper
-    cmd = self.gripper_cmd
-    if msg.close_gripper:
-      cmd = 5.0
-    else:
-      cmd = -5.0
-    try:
-      self.gripper_pub.publish(cmd)
-    except rospy.exceptions.ROSException:
-      pass
   
   def cb_slave_state(self, msg):
     self.slave_pos = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
     self.slave_rot = np.array([msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w])
+  
+  def buttons_cb(self, msg):
+    button_states = [msg.grey_button, msg.white_button]
+    # Check that any button was pressed / released
+    for i, previous in enumerate(self.prev_buttons):
+      if (previous != button_states[i]) and button_states[i] == 1:
+        self.buttons[i] = not self.buttons[i]
+    # Open or close the gripper
+    if self.buttons[GREY_BUTTON]:   # Close
+      self.gripper_cmd = GRIPPER_RATIO
+    else:                           # Open
+      self.gripper_cmd = -GRIPPER_RATIO
+    self.gripper_pub.publish(Float64(self.gripper_cmd))
   
   def publish_command(self, event):
     position, orientation = self.command_pos, self.command_rot
     ik_mc_msg = PoseStamped()
     ik_mc_msg.header.frame_id = self.frame_id
     ik_mc_msg.header.stamp = rospy.Time.now()
-    ik_mc_msg.pose.position.x = position[0]
-    ik_mc_msg.pose.position.y = position[1]
-    ik_mc_msg.pose.position.z = position[2]
-    ik_mc_msg.pose.orientation.x = orientation[0]
-    ik_mc_msg.pose.orientation.y = orientation[1]
-    ik_mc_msg.pose.orientation.z = orientation[2]
-    ik_mc_msg.pose.orientation.w = orientation[3]
+    ik_mc_msg.pose.position = Point(*position)
+    ik_mc_msg.pose.orientation = Quaternion(*orientation)
     try:
       self.ik_mc_pub.publish(ik_mc_msg)
       self.send_feedback()
